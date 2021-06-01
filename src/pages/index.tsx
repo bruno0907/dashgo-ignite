6c3813f7aa0a29237c7f72ccf3ec1da0f472010e
bruno0907 <bruno0907@gmail.com>
2021-06-01 15:53:11 -0300

    chore: added typscript. added chakraui. added _document file. added theme and theme provider. added signin page

---
+++ src/pages/index.tsx
@@ -0,0 +1,61 @@
+import { Flex, Input, FormLabel, FormControl, Button, Stack } from '@chakra-ui/react';
+
+export default function Home() {
+  return (
+    <Flex
+      w="100vw"
+      h="100vh"
+      align="center"
+      justify="center"
+    >
+      <Flex
+        as="form"
+        w="100%"
+        maxW="360px"
+        bg="gray.800"
+        p="8"
+        borderRadius="8px"
+        flexDir="column"
+      >
+        <Stack spacing="4">
+          <FormControl>
+            <FormLabel htmlFor="email">Email</FormLabel>
+            <Input
+              id="email"
+              name="email"
+              type="email" 
+              focusBorderColor="pink.500"
+              bgColor="gray.900"
+              variant="filled"
+              _hover={{
+                bgColor: "gray.900"
+              }}
+              size="lg"
+            />
+          </FormControl>
+          <FormControl>
+            <FormLabel htmlFor="password">Senha</FormLabel>
+            <Input
+              id="password"
+              name="password"
+              type="password" 
+              focusBorderColor="pink.500"
+              bgColor="gray.900"
+              variant="filled"
+              _hover={{
+                bgColor: "gray.900"
+              }}
+              size="lg"
+            />
+          </FormControl>
+        </Stack>
+        <Button
+          type="submit"
+          mt="6"
+          colorScheme="pink"
+          size="lg"
+        >Entrar</Button>
+      </Flex>      
+    </Flex>
+  )
+}
