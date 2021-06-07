c8f8d4b57d2364fd6964ab66a5dc49eb1642e223
bruno0907 <bruno0907@gmail.com>
2021-06-07 12:55:12 -0300

    feat: added form handling and validation with react-hook-form and yup to signin and create user pages

--- src/components/Form/Input.tsx
+++ src/components/Form/Input.tsx
@@ -1,29 +1,45 @@
-import { FormControl, Input as ChakraInput, InputProps as ChakraInputProps, FormLabel } from "@chakra-ui/react";
-import React from "react";
+import { 
+  FormControl, 
+  Input as ChakraInput, 
+  InputProps as ChakraInputProps, 
+  FormLabel,
+  FormErrorMessage,
+} from "@chakra-ui/react";
+import React, { forwardRef, ForwardRefRenderFunction } from "react";
+import { FieldError } from "react-hook-form";
 
 interface InputProps extends ChakraInputProps{
   name: string;
   label?: string;
+  error?: FieldError;
 }
 
-function Input({ name, label, ...rest }: InputProps) {
-  return (
-    <FormControl>
-      { label && <FormLabel htmlFor={name}>{label}</FormLabel>}
-      <ChakraInput
-        id={name}
-        name={name}
-        focusBorderColor="pink.500"
-        bgColor="gray.900"
-        variant="filled"
-        _hover={{
-          bgColor: "gray.900"
-        }}
-        size="lg"
-        {...rest}
-      />
-    </FormControl>
-  )
+const InputBase: ForwardRefRenderFunction<HTMLInputElement, InputProps> = 
+  ({ name, label, error = null, ...rest }, ref) => {
+    return (
+      <FormControl isInvalid={!!error}>
+        { label && <FormLabel htmlFor={name}>{label}</FormLabel>}
+        <ChakraInput
+          id={name}
+          name={name}
+          focusBorderColor="pink.500"
+          bgColor="gray.900"
+          variant="filled"
+          _hover={{
+            bgColor: "gray.900"
+          }}
+          size="lg"
+          ref={ref}
+          error={error}
+          {...rest}
+        />
+        { !!error && (
+            <FormErrorMessage>{error.message}</FormErrorMessage>
+        )}
+      </FormControl>
+    )
 }
+
+const Input = forwardRef(InputBase);
 
 export { Input };
